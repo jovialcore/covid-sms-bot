8135c1db599ea56f2fb481d4afd81badfd7b864c
jovialcore <jollydosky@gmail.com>
2020-04-15 21:38:33 -0700

    done with the bot

--- app/Http/Controllers/covidSmsBot.php
+++ app/Http/Controllers/covidSmsBot.php
@@ -10,15 +10,32 @@ use Twilio\Rest\Client;
 
 class covidSmsBot extends Controller
 {
-    public functio countryCase (Request $request) {
+    public function countryCase(Request $request) {
 
     	$userNum = $request->input('from');
     	$countryNameSentByUser = $request->input('body');
-    	$response = Http::get("https://scorona.lmao.ninja/countries/{$countryNameSentByUser}")l 
+    	$response = Http::get("https://scorona.lmao.ninja/countries/{$countryNameSentByUser}");
+    	$response =  json_decode($response->body());
+
+    	if(isset($response->message)){
+    		$this->wireMessage($response->message, $userNum);
+    		return;
+    	}; 
 
 
 
-    	private function wireMessage ($theReplies, $reciever) {
+    		$theReplies = "Hi 👋  Summary of covid-19 cases in" . Str::title($countryNameSentByUser). "as at". now()->toRfc850String() . "\n\n";
+			
+			$theReplies.="Today's cases: {$response->todayCases} \n";
+			$theReplies.="Recoverd Cases: {$response->recovered} \n";
+			$theReplies.="Deaths Recorded: {$response->deaths} \n";
+			$theReplies.="total cases: {$response}";
+
+			$this->wireMessage($theReplies, $userNum);
+				return;
+		}
+
+    	private function wireMessage($theReplies, $reciever) {
 
     		$act_sid = getenv("TWILIO_ACCOUNT_SID");
     		$auth_token = getenv("TWILIO_AUTH_TOKEN");
@@ -26,8 +43,12 @@ class covidSmsBot extends Controller
 
     		$twiloClientClassInst = new Client ($act_sid, $auth_token );
 
-    		return $twiloClientClassInst->messages->create($reciever, ['from'=> $twilo_num, 'body'= $theReplies ]) 
-    	} 
+    		return $twiloClientClassInst->messages->create($reciever, ['from'=> $twilo_num, 'body'=> $theReplies]); 
+    		//lets check if the onject json has some value
+    	}
+
+
+
 
     }
-}
+
